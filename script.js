7e4130f58bb916dd3c2acd365d3d50a7fc38a329
James Tomasino <git@jamestomasino.com>
2012-06-20 17:28:11 -0400

    Now listening for enter and backspace

--- script.js
+++ script.js
@@ -4,17 +4,20 @@ Namespace.import ( this, 'org.tomasino.utils.Keyboard' );
 var obscure = new ObscureText();
 var keyboard = new Keyboard( document );
 keyboard.addEventListener ( Keyboard.EVENT_KEY_UP, onKeyUp );
+keyboard.addEventListener ( Keyboard.EVENT_BACKSPACE_UP, onBackspace );
+keyboard.addEventListener ( Keyboard.EVENT_ENTER_UP, onEnter );
 
 function onKeyUp ( keyObj ) {
 	var enc = obscure.charByCode ( keyObj.charCode );
 	$('section').append ( enc );
 }
 
-/*
-			$('section').append ( '\n' );
+function onBackspace ( ) {
+	v = $('section').html();
+	v = v.substring ( 0, v.length - 1);
+	$('section').html(v);
+}
 
-			v = $('section').html();
-			v = v.substring ( 0, v.length - 1);
-			$('section').html(v);
-
-*/
+function onEnter ( ) {
+	$('section').append ( '\n' );
+}
