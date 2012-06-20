6e7be479afebe4b30450842dc286fd21b772e468
James Tomasino <git@jamestomasino.com>
2012-06-20 13:36:02 -0400

    support special characters

--- org/tomasino/utils/ObscureText.class.js
+++ org/tomasino/utils/ObscureText.class.js
@@ -6,6 +6,42 @@
 
 		constructor: function( ) {
 			this.chars      = [];
+
+			this.chars[33]  = ['¡','!'];
+			this.chars[34]  = ['“','”','"'];
+			this.chars[35]  = ['#'];
+			this.chars[36]  = ['$'];
+			this.chars[37]  = ['%'];
+			this.chars[38]  = ['&'];
+			this.chars[39]  = ['\''];
+			this.chars[40]  = ['('];
+			this.chars[41]  = [')'];
+			this.chars[42]  = ['*'];
+			this.chars[43]  = ['+'];
+			this.chars[44]  = ['„','‚','¸'];
+			this.chars[45]  = ['–', '—', '¬', '¯'];
+			this.chars[46]  = ['•', '·'];
+			this.chars[47]  = ['/'];
+
+			this.chars[48]  = ['Ø','ø','0'];
+			this.chars[49]  = ['1'];
+			this.chars[50]  = ['2'];
+			this.chars[51]  = ['3'];
+			this.chars[52]  = ['4'];
+			this.chars[53]  = ['5'];
+			this.chars[54]  = ['6'];
+			this.chars[55]  = ['7'];
+			this.chars[56]  = ['8'];
+			this.chars[57]  = ['9'];
+
+			this.chars[58]  = [':'];
+			this.chars[59]  = [';'];
+			this.chars[60]  = ['<'];
+			this.chars[61]  = ['='];
+			this.chars[62]  = ['>'];
+			this.chars[63]  = ['¿','?'];
+			this.chars[64]  = ['@'];
+
 			this.chars[65]  = ['À','Á','Â','Ã','Ä','Å'];
 			this.chars[66]  = ['ß','Β'];
 			this.chars[67]  = ['Ç','Ĉ','Ċ'];
@@ -24,8 +60,15 @@
 			this.chars[84]  = ['Ŧ'];
 			this.chars[85]  = ['Ù','Ú','Û','Ü','Ũ','Ū','Ŭ','Ů','Ű','Ų'];
 			this.chars[87]  = ['Ŵ'];
-			this.chars[89]  = ['Ý','Ÿ','Ŷ','Ÿ'];
+			this.chars[89]  = ['Ý','Ÿ','Ŷ','Ÿ','¥'];
 			this.chars[90]  = ['Ž'];
+
+			this.chars[91]  = ['['];
+			this.chars[92]  = ['\\'];
+			this.chars[93]  = [']'];
+			this.chars[94]  = ['^'];
+			this.chars[95]  = ['_'];
+			this.chars[96]  = ['`'];
 
 			this.chars[97]  = ['à','á','â','ã','ä','å','ą'];
 			this.chars[98]  = ['þ'];
@@ -48,6 +91,12 @@
 			this.chars[120] = ['×'];
 			this.chars[121] = ['ý','ÿ'];
 			this.chars[122] = ['ź','ż','ž'];
+
+			this.chars[123]  = ['{'];
+			this.chars[124]  = ['|'];
+			this.chars[125]  = ['}'];
+			this.chars[126]  = ['~'];
+
 		},
 
 		charByCode: function ( num ) {
--- script.js
+++ script.js
@@ -15,28 +15,104 @@ var capslock = false;
 
 function onKeyUp ( e ) {
 	var k = (window.event) ? event.keyCode : e.keyCode;
+	console.log (k);
 	switch (k)
 	{
 		case 8: // backspace
+			return;
 			break;
 		case 20: // capslock
+			return;
 			break;
 		case 16: // shift
 			shift = false;
+			return;
 			break;
 		case 13: // enter
 			$('section').append ( '\n' );
+			return;
 			break;
+		case 48: // 0
+			if (shift) k = 41;
+			break;
+		case 49: // 1
+			if (shift) k = 33;
+			break;
+		case 50: // 2
+			if (shift) k = 64;
+			break;
+		case 51: // 3
+			if (shift) k = 35;
+			break;
+		case 52: // 4
+			if (shift) k = 36;
+			break;
+		case 53: // 5
+			if (shift) k = 37;
+			break;
+		case 54: // 6
+			if (shift) k = 94;
+			break;
+		case 55: // 7
+			if (shift) k = 38;
+			break;
+		case 56: // 8
+			if (shift) k = 42;
+			break;
+		case 57: // 9
+			if (shift) k = 40;
+			break;
+		case 44: // ,
+		case 188: // should be , but is ¼
+			k = (shift) ? 60 : 44;
+			break;
+		case 46: // .
+		case 190: // should be . but is ¾
+			k = (shift) ? 62 : 46;
+			break;
+		case 39: // '
+		case 222: // should be ' but is Þ
+			k = (shift) ? 34 : 39;
+			break;
+		case 59: // ;
+		case 186: // º
+			k = (shift) ? 58 : 59;
+			break;
+		case 91:
+		case 219: // [
+			k = (shift) ? 123 : 91;
+			break;
+		case 93:
+		case 221: // ]
+			k = (shift) ? 125 : 93;
+			break;
+		case 47:
+		case 191: // /
+			k = (shift) ? 63 : 47;
+			break;
+		case 61:
+		case 187: // =
+			k = (shift) ? 43 : 61;
+			break;
+		case 39:
+		case 220: // \
+			k = (shift) ? 124 : 39;
+			break;
+		case 45:
+		case 189: // -
+			k = (shift) ? 95 : 45;
+			break;
+
 		default:
 			if (!shift && (k >= 65 && k <= 90))
 			{
 				k += 32;
 			}
-
-			var enc = obscure.charByCode ( k );
-			$('section').append ( enc );
 			break;
 	}
+	var enc = obscure.charByCode ( k );
+	$('section').append ( enc );
+
 	e.preventDefault();
 }
 
@@ -63,17 +139,3 @@ function onKeyDown ( e ) {
 	}
 	e.preventDefault();
 }
-
-function getChar(event) {
-  if (event.which == null) {
-	console.log ('ie');
-    return String.fromCharCode(event.keyCode) // IE
-  } else if (event.which != 0 && event.charCode != 0) {
-	console.log ('rest');
-    return String.fromCharCode(event.which)   // the rest
-  } else {
-	console.log ('special');
-    return null // special key
-  }
-}
-
